9da41e56a26bede06d8442061e87af39a60bcafe
JOYMALAKAR7 <malakarjoy4@gmail.com>
2023-08-29 16:23:53 +0530

    RegisterScreen DOne

--- App.tsx
+++ App.tsx
@@ -2,14 +2,21 @@ import { View, Text, StatusBar } from 'react-native'
 import React from 'react'
 import LoginScreen from './src/screens/LoginScreen'
 import { Colors } from './src/utlis/Colors'
-
-
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+import RegisterScreen from './src/screens/RegisterScreen';
+const Stack = createStackNavigator();
 const App = () => {
   return (
-    <View>
-      <StatusBar backgroundColor={Colors.white}barStyle={'dark-content'} />
-      <LoginScreen />
-    </View>
+    <NavigationContainer>
+        <StatusBar backgroundColor={Colors.white}barStyle={'dark-content'} />
+      <Stack.Navigator screenOptions={{headerShown:false}}>
+    
+        <Stack.Screen name='LoginScreen' component={LoginScreen} />
+        <Stack.Screen name='RegisterScreen' component={RegisterScreen} />
+       </Stack.Navigator>
+     
+      </NavigationContainer>
   )
 }
 
--- src/screens/LoginScreen.jsx
+++ src/screens/LoginScreen.jsx
@@ -1,100 +1,118 @@
-import { View, Text, Image ,StyleSheet, TextInput, TouchableOpacity} from 'react-native'
-import React, { useState } from 'react'
-import VectorIcons from '../utlis/VectorIcons'
-import { Colors } from '../utlis/Colors'
-import Logo from '../assets/images/logo.png'
-import MetaLogo from '../assets/images/meta-logo.png'
-const LoginScreen = () => {
-    const [email,setEmail]=useState('')
-    const [password,setPassword]=useState('')
-    return (
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native';
+import React, {useState} from 'react';
+import VectorIcons from '../utlis/VectorIcons';
+import {Colors} from '../utlis/Colors';
+import Logo from '../assets/images/logo.png';
+import MetaLogo from '../assets/images/meta-logo.png';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+const Stack = createStackNavigator();
+const LoginScreen = ({navigation}) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const onCreateAccount =()=>{
+    navigation.navigate("RegisterScreen")
+  }
+  return (
     <View style={styles.Container}>
-        <VectorIcons name='arrow-back' size={20} color={Colors.black} type='Ionicons'  />
-        <View style={styles.subContainer}>
+      <VectorIcons
+        name="arrow-back"
+        size={20}
+        color={Colors.black}
+        type="Ionicons"
+      />
+      <View style={styles.subContainer}>
         <Image source={Logo} style={styles.logoStyle} />
-        <TextInput 
-        placeholder='Mobile Number Or Email'
-        value={email}
-        onChangeText={value=>setEmail(value)}
-        style={styles.textInoutBox}
+        <TextInput
+          placeholder="Mobile Number Or Email"
+          value={email}
+          onChangeText={value => setEmail(value)}
+          style={styles.textInoutBox}
         />
-<TextInput 
-        placeholder='Password'
-        value={password}
-        onChangeText={value=>setPassword(value)}
-        style={styles.textInoutBox}
-        
+        <TextInput
+          placeholder="Password"
+          value={password}
+          onChangeText={value => setPassword(value)}
+          style={styles.textInoutBox}
+          secureTextEntry={true}
         />
         <TouchableOpacity style={styles.loginButton}>
-            <Text style={styles.login}>Log In</Text>
+          <Text style={styles.login}>Log In</Text>
         </TouchableOpacity>
         <Text style={styles.forgotPassword}>Forgot Password?</Text>
-        <TouchableOpacity style={styles.newAccount}>
-            <Text style={styles.newAccountText}>Create new account?</Text>
+        <TouchableOpacity style={styles.newAccount} onPress={onCreateAccount}>
+          <Text style={styles.newAccountText}>Create new account?</Text>
         </TouchableOpacity>
         <Image source={MetaLogo} style={styles.MetaLogo} />
-        </View>
-        
+      </View>
     </View>
-  )
-}
-const styles=StyleSheet.create({
-    Container:{
-        padding:16
-    },
-    subContainer:{
-justifyContent:'center',
+  );
+};
+const styles = StyleSheet.create({
+  Container: {
+    padding: 16,
+  },
+  subContainer: {
+    justifyContent: 'center',
 
-alignItems:'center'
-    },
-    logoStyle:{
-height:50,
-width:50,
-marginVertical:'20%'
-    },
-    textInoutBox:{
-        borderColor:Colors.grey,
-        borderWidth:1,
-        borderRadius:10,
-        width:'95%',
-        margin:10
-    },
-    loginButton:{
-        backgroundColor:Colors.primaryColor,
-        width:'95%',padding:10,
-        borderRadius:20,
-        alignItems:'center',
-        marginTop:12
-    },
-login:{
-    color:Colors.white,
-    fontSize:15,
-    fontWeight:'500'
-},
-forgotPassword:{
-    color:Colors.grey,
-    fontSize:14,
-    fontWeight:'500' ,
-    marginTop:15
-},
-newAccount:{
-    borderColor:Colors.primaryColor,
-    borderWidth:1,
-        width:'95%',
-        padding:10,
-        borderRadius:20,
-        alignItems:'center',
-        marginTop:"35%"
-},
-newAccountText:{
-    color:Colors.primaryColor,
-    fontSize:14,
-    fontWeight:'500'
-},
-MetaLogo:{
-    height:15,
-width:70,
-marginTop:15
-}
-})
-export default LoginScreen 
+    alignItems: 'center',
+  },
+  logoStyle: {
+    height: 50,
+    width: 50,
+    marginVertical: '20%',
+  },
+  textInoutBox: {
+    borderColor: Colors.grey,
+    borderWidth: 1,
+    borderRadius: 10,
+    width: '95%',
+    margin: 10,
+  },
+  loginButton: {
+    backgroundColor: Colors.primaryColor,
+    width: '95%',
+    padding: 10,
+    borderRadius: 20,
+    alignItems: 'center',
+    marginTop: 12,
+  },
+  login: {
+    color: Colors.white,
+    fontSize: 15,
+    fontWeight: '500',
+  },
+  forgotPassword: {
+    color: Colors.grey,
+    fontSize: 14,
+    fontWeight: '500',
+    marginTop: 15,
+  },
+  newAccount: {
+    borderColor: Colors.primaryColor,
+    borderWidth: 1,
+    width: '95%',
+    padding: 10,
+    borderRadius: 20,
+    alignItems: 'center',
+    marginTop: '35%',
+  },
+  newAccountText: {
+    color: Colors.primaryColor,
+    fontSize: 14,
+    fontWeight: '500',
+  },
+  MetaLogo: {
+    height: 15,
+    width: 70,
+    marginTop: 15,
+  },
+});
+export default LoginScreen;
